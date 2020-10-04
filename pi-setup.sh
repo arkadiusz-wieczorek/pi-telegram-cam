ef154be825020cb83edef66d8c52a772262e6616
Arkadiusz Wieczorek 🚀 <kontakt@arkadiusz-wieczorek.pl>
2020-10-04 21:45:09 +0200

    Update pi-setup.sh

--- pi-setup.sh
+++ pi-setup.sh
@@ -1,4 +1,6 @@
-sudo apt-get install fswebcam
-sudo apt-get install v4l-utils
+git stash
+git pull
+git reset HEAD --hard
+sudo apt-get install fswebcam v4l-utils
 sudo modprobe bcm2835-v4l2
 npm install --save raspi raspi-serial
