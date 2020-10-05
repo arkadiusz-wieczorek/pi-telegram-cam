079bb59ac350078f6644d8d79b58c96eb9a7db83
Arkadiusz Wieczorek 🚀 <kontakt@arkadiusz-wieczorek.pl>
2020-10-05 17:21:36 +0200

    Add output for event serial

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ const TOKEN = process.env.TELEGRAM_TOKEN;
 const USER_1 = process.env.USER_1;
 const USER_2 = process.env.USER_2;
 const bot = new tg(TOKEN, {
-	polling: true,
-	filepath: false,
+    polling: true,
+    filepath: false,
 });
 
 // SERIAL PORT CONFIG
@@ -22,49 +22,50 @@ bot.sendMessage(USER_1, `Restart server "pi-telegram-cam" - ${new Date()}`);
 
 // SERIAL PORT INIT
 raspi.init(() => {
-	serial.open(() => {
-		serial.on("data", (data) => {
-			emitter.emit("event", data.toString("utf8"));
-		});
-	});
+    serial.open(() => {
+        serial.on("data", (data) => {
+            emitter.emit("event", data.toString("utf8"));
+        });
+    });
 });
 
 emitter.on("event", (data) => {
-	dispatch(USER_1, false);
+    console.log("event →", data);
+    dispatch(USER_1, false);
 });
 
 bot.onText(/\/photo/, (data) => {
-	console.log(`data.chat.id: ${data.chat.id}`);
+    console.log(`data.chat.id: ${data.chat.id}`);
 
-	if (data.chat.id == USER_1 || data.chat.id == USER_2) {
-		dispatch(data.chat.id, false);
-	} else {
-		bot.sendMessage(
-			`Block user id: ${data.chat.id}, first_name: ${data.chat.first_name}`
-		);
-	}
+    if (data.chat.id == USER_1 || data.chat.id == USER_2) {
+        dispatch(data.chat.id, false);
+    } else {
+        bot.sendMessage(
+            `Block user id: ${data.chat.id}, first_name: ${data.chat.first_name}`
+        );
+    }
 });
 
 function dispatch(chatId, toAll = false) {
-	getImage("/dev/video0", 640, 480, "1").then((data) =>
-		sendPhoto(data, toAll)
-	);
-	// getImage("/dev/video1", 640, 480, "1").then((data) =>
-	// 	sendPhoto(data, toAll)
-	// );
+    getImage("/dev/video0", 640, 480, "1").then((data) =>
+        sendPhoto(data, toAll)
+    );
+    // getImage("/dev/video1", 640, 480, "1").then((data) =>
+    // 	sendPhoto(data, toAll)
+    // );
 }
 
 function sendPhoto(data, toAll = false) {
-	if (!toAll) {
-		bot.sendPhoto(USER_1, data, {
-			caption: new Date(),
-		});
-	} else {
-		bot.sendPhoto(USER_1, data, {
-			caption: new Date(),
-		});
-		bot.sendPhoto(USER_2, data, {
-			caption: new Date(),
-		});
-	}
+    if (!toAll) {
+        bot.sendPhoto(USER_1, data, {
+            caption: new Date(),
+        });
+    } else {
+        bot.sendPhoto(USER_1, data, {
+            caption: new Date(),
+        });
+        bot.sendPhoto(USER_2, data, {
+            caption: new Date(),
+        });
+    }
 }
