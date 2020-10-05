d296a8c62604c866ea1f8362872ebf484edcf741
Arkadiusz Wieczorek 🚀 <kontakt@arkadiusz-wieczorek.pl>
2020-10-05 18:37:18 +0200

    Update scripts

---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt-get install fswebcam v4l-utils
+sudo modprobe bcm2835-v4l2
--- sync.sh
+++ sync.sh
@@ -1,6 +1,5 @@
+#!/bin/bash
 git stash
 git pull
 git reset HEAD --hard
-sudo apt-get install fswebcam v4l-utils
-sudo modprobe bcm2835-v4l2
 npm install --save raspi raspi-serial
