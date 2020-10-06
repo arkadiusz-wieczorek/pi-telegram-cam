7baccad1a488f979f5cedd5399fcb1564f0da2fc
Arkadiusz Wieczorek 🚀 <kontakt@arkadiusz-wieczorek.pl>
2020-10-06 10:26:15 +0200

    Revert config

--- src/components/camera.js
+++ src/components/camera.js
@@ -2,19 +2,21 @@ const NodeWebcam = require("node-webcam");
 const opts = {
 	quality: 100,
 	delay: 0,
-	saveShots: false,
+	saveShots: true,
 	output: "jpeg",
 	callbackReturn: "buffer",
-	verbose: true,
+	verbose: false,
 };
 
 module.exports = function getImage(cameraPath, width, height, filename) {
+	const device = cameraPath;
+
 	return new Promise((resolve, reject) => {
-		console.log(`camera path: ${cameraPath} ${filename}`);
+		console.log(`camera path: ${device} ${filename}`);
 
 		NodeWebcam.capture(
 			filename,
-			{ ...opts, cameraPath, width, height },
+			{ ...opts, device, width, height },
 			(err, data) => {
 				if (err) reject("error", err);
 				resolve(data);
